f057215a97b1adf575cb7c3c8ed46f16b317b7b1
lancelot1337 <sudhanshusah2001@gmail.com>
2018-07-24 15:39:56 +0530

    Add validation for startsat and endsat in events table

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
     validates :title, presence: true, length: { minimum: 3, maximum: 20 }
     validates :description, presence: true, length: { minimum: 5, maximum: 100 }
     validates :venue, presence: true
+    validates :startsat, presence: true
+    validates :endsat, presence: true
 end
