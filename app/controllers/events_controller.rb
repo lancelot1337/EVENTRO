35606d0416bfa7385bc29ee87f1c0b35dbcb9b4e
lancelot1337 <sudhanshusah2001@gmail.com>
2018-07-24 15:39:29 +0530

    Add create and show actions; and show template

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,26 @@ class EventsController < ApplicationController
     def new
         @event = Event.new
     end
+
+    def create
+        @event = Event.new(event_params)
+        if @event.save
+            flash[:success] = "Successfully created the event!"
+            redirect_to event_path(@event)
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        @event = Event.find(params[:id])
+    end
+
+    private
+    def event_params
+        params.require(:event).permit(:title, :description, :venue, :startsat, :endsat)
+    end
+    def events_show
+
+    end
 end
