00ab26ca11426a9985e02adc1473f5d8b9b19cf4
lancelot1337 <sudhanshusah2001@gmail.com>
2018-07-25 17:36:33 +0530

    Build organizer-event otm association

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,9 @@
 class Event < ApplicationRecord
+    belongs_to :organizer
     validates :title, presence: true, length: { minimum: 3, maximum: 20 }
     validates :description, presence: true, length: { minimum: 5, maximum: 100 }
     validates :venue, presence: true
     validates :startsat, presence: true
     validates :endsat, presence: true
+    validates :organizer_id, presence: true
 end
--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -1,4 +1,6 @@
 class Organizer<ApplicationRecord
+    has_many :events
+    before_save {self.email = email.downcase}
     validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 30}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 50}, format: {with: VALID_EMAIL_REGEX}
