1a1e5d540b8df54830d7cf118298b717d8109089
lancelot1337 <sudhanshusah2001@gmail.com>
2018-07-29 14:03:57 +0530

    Add code for create and destroy action in sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,22 @@ class SessionsController < ApplicationController
     end
 
     def create
-
+        organizer = Organizer.find_by(email: params[:session][:email].downcase)
+        if organizer && organizer.authenticate(params[:session][:password])
+            # using sessions to log in
+            session[:organizer_id] = organizer.id            
+            flash[:success] = "You are logged in!"
+            redirect_to organizer_path(organizer)
+        else
+            #flash persists for one http request, flash.now doesn't
+            flash.now[:danger] = "Incorrect credentials"
+           render 'new' 
+        end
     end
     
     def destroy
-
+        session[:organizer_id] = nil
+        flash[:success] = "You have logged out!"
+        redirect_to root_path
     end
 end
