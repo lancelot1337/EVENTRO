baa0a5cf1c544bc4eccde62387e286ead9147de5
lancelot1337 <sudhanshusah2001@gmail.com>
2018-07-29 14:26:32 +0530

    Add helper methods which are available to templates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+    # to make them accessible by views
+    helper_method :current_organizer, :logged_in?
+
+    def current_organizer
+        # return the current organizer
+        # Organizer.find(session[:organizer_id]) if session[:organizer_id]
+
+        # using memoization
+        @current_organizer  ||= Organizer.find(session[:organizer_id]) if session[:organizer_id]
+    end
+
+    def logged_in?
+        # return true if current_organizer exists
+        !!current_organizer
+    end
+
+    def require_organizer
+        if !logged_in?
+            flash[:danger] = "You must be logged in to perform that action"
+            redirect_to root_path
+        end
+    end
 end
