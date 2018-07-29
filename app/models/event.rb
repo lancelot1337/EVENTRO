12a222abfaa986565c0983121d66a31938d323b2
lancelot1337 <sudhanshusah2001@gmail.com>
2018-07-29 16:14:51 +0530

    Correctly order the events index page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
+    default_scope { order(created_at: :desc) }
     belongs_to :organizer
     validates :title, presence: true, length: { minimum: 3, maximum: 20 }
     validates :description, presence: true, length: { minimum: 5, maximum: 100 }
