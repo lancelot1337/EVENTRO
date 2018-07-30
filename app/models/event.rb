b172615808ec15f877f4a770c73051879989cdd7
lancelot1337 <sudhanshusah2001@gmail.com>
2018-07-30 23:58:51 +0530

    Update events' title and description validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
     default_scope { order(created_at: :desc) }
     belongs_to :organizer
-    validates :title, presence: true, length: { minimum: 3, maximum: 20 }
-    validates :description, presence: true, length: { minimum: 5, maximum: 100 }
+    validates :title, presence: true, length: { minimum: 3, maximum: 100 }
+    validates :description, presence: true, length: { minimum: 5, maximum: 5000 }
     validates :venue, presence: true
     validates :startsat, presence: true
     validates :endsat, presence: true
